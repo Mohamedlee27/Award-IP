e3234022b4563090917e81027eb4e154a988e624
Mohamedlee27 <leonk8070@gmail.com>
2022-06-14 16:25:04 +0300

    created url file and added new def in models.py

--- accounts/models.py
+++ accounts/models.py
@@ -24,3 +24,20 @@ class MyAccountManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
+
+
+    def create_superuser(self, first_name, last_name, email, username, password):
+        user = self.create_user(
+            email  = self.normalize_email(email),
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
+            )
+            
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
