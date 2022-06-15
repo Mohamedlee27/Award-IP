2963bb394458e44dde09d8ce488e6606defcdc45
Mohamedlee27 <leonk8070@gmail.com>
2022-06-15 07:49:52 +0300

    created imports and register and login functionalities

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render,get_object_or_404
+from accounts.models import Account
+from .forms import RegistrationForm,UserForm,UserProfileForm
+from.models import Account,UserProfile
+from django.contrib import messages,auth
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login,logout,authenticate
+
+
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user = Account.objects.create_user(first_name=first_name,last_name=last_name,
+                                                email=email,username=username,password=password)
+            user.phone_number = phone_number
+            user.is_active = True
+            user.save()
+
+            # Create a user profile
+            profile = UserProfile()
+            profile.user_id = user.id
+            profile.profile_picture = 'default/default-user.jpg'
+            profile.save()
+
+
+
+
+            
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/register.html' ,context)
+
+
+def login_user(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = authenticate(request,email=email,password=password)
+
+        if user is not None:
+            login(request,user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
+    return render(request, 'accounts/login.html')
