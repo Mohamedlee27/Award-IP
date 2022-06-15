168961944f4d319f8d0fb31190c9097143b52c3a
Mohamedlee27 <leonk8070@gmail.com>
2022-06-15 07:55:52 +0300

    created url paths

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,11 @@
 from django.urls import path
 from  .import views
 
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+
+
+]
